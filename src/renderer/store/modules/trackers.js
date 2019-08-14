e37b79f07645043bc98342d0121358b2b95fcf0a
ktmouk <ktmouk@gmail.com>
2019-08-14 21:06:42 +0900

    testを変更

--- src/renderer/store/modules/trackers.js
+++ src/renderer/store/modules/trackers.js
@@ -1,28 +1,28 @@
 import { tracker } from '../schemas';
 import uniqid from 'uniqid';
 
-export const SET_ACTIVED = 'SET_ACTIVED';
+export const SET_STARTED = 'SET_STARTED';
 export const SET_STOP_ALL_ON_SUSPEND = 'SET_STOP_ALL_ON_SUSPEND';
 export const SET_STOP_ALL_ON_SHUTDOWN = 'SET_STOP_ALL_ON_SHUTDOWN';
 
 export const state = () => ({
-  actived: undefined,
+  started: undefined,
   stopAllOnSuspend: true,
   stopAllOnShutdown: true
 });
 
 export const actions = {
   update({ state, commit, dispatch, getters, rootGetters }) {
-    if (getters.active && !state.actived) dispatch('start');
-    if (!getters.active && state.actived) dispatch('stop');
-    commit(SET_ACTIVED, !!getters.active);
+    if (getters.tracking && !state.started) dispatch('start');
+    if (!getters.tracking && state.started) dispatch('stop');
+    commit(SET_STARTED, !!getters.tracking);
   },
   start({ dispatch, getters }) {
     dispatch(
       'activities/add',
       {
-        projectId: getters.active.project.id,
-        description: getters.active.description,
+        projectId: getters.tracking.project.id,
+        description: getters.tracking.description,
         startedAt: `${new Date()}`
       },
       { root: true }
@@ -59,8 +59,8 @@ export const actions = {
 };
 
 export const mutations = {
-  [SET_ACTIVED](state, tracker) {
-    state.actived = tracker;
+  [SET_STARTED](state, started) {
+    state.started = started;
   },
   [SET_STOP_ALL_ON_SUSPEND](state, payload) {
     state.stopAllOnSuspend = payload;
@@ -76,7 +76,7 @@ export const getters = {
       tracker => tracker.project !== undefined
     );
   },
-  active(state, getters, rootState, rootGetters) {
+  tracking(state, getters, rootState, rootGetters) {
     const processes = rootGetters['processes/all'];
     return getters.all.find(tracker => processes.includes(tracker.process));
   },
