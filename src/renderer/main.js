5b85aa374d339fe8b737de49266e3919607f8b6d
ktmouk <ktmouk@gmail.com>
2019-08-20 18:10:19 +0900

    v-tooltipを追加

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 import VueTimers from 'vue-timers';
 import VueElectron from 'vue-electron';
+import VTooltip from 'v-tooltip';
 import App from './app';
 import router from './router';
 import store from './store';
@@ -13,6 +14,7 @@ Vue.config.productionTip = false;
 Vue.use(VueElectron);
 Vue.use(VueTimers);
 Vue.use(VueI18n);
+Vue.use(VTooltip);
 
 new Vue({
   components: { App },
