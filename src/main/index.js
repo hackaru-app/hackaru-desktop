a621df5f22ff478019c8de01e9ad39b8e97ca59c
ktmouk <ktmouk@gmail.com>
2019-08-23 18:51:22 +0900

    envsを追加

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,4 @@
+import './envs';
 import '../renderer/store';
 import './windows';
 import './trackers';
@@ -6,16 +7,6 @@ import './oauth-window';
 import './power-monitor';
 import './persistor';
 import './menubar';
-
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path')
-    .join(__dirname, '/static')
-    .replace(/\\/g, '\\\\');
-}
 
 /**
  * Auto Updater
