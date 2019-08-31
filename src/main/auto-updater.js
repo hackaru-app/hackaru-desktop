04e0b344d1f44312e2d8b9225b024157d16999f9
ktmouk <ktmouk@gmail.com>
2019-08-31 15:16:29 +0900

    electron-logを追加

--- src/main/auto-updater.js
+++ src/main/auto-updater.js
@@ -1,8 +1,10 @@
 import { app } from 'electron';
 import { autoUpdater } from 'electron-updater';
+import log from 'electron-log';
 
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') {
+    autoUpdater.logger = log;
     autoUpdater.checkForUpdatesAndNotify();
   }
 });
