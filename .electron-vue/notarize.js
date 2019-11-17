592f81a0c17f0abc0846dd223dba187f8d4defd5
ktmouk <ktmouk@gmail.com>
2019-11-17 20:04:28 +0900

    CURRENT_BRANCHに変更

--- .electron-vue/notarize.js
+++ .electron-vue/notarize.js
@@ -3,7 +3,7 @@ const { notarize } = require('electron-notarize')
 
 const appleId = process.env.APPLE_ID
 const appleIdPassword = process.env.APPLE_PASSWORD
-const master = process.env.TRAVIS_CURRENT_BRANCH === 'master'
+const master = process.env.CURRENT_BRANCH === 'master'
 
 const configPath = path.resolve(__dirname, '../package.json')
 const appPath = path.resolve(__dirname, '../build/mac/Hackaru.app')
@@ -11,7 +11,7 @@ const config = require(configPath)
 const appBundleId = config.build.appId
 
 exports.default = async () => {
-  console.warn(`debug: ${process.env.TRAVIS_CURRENT_BRANCH}`);
+  console.warn(`debug: ${process.env.CURRENT_BRANCH}`);
   if (!appleId || !appleIdPassword || !master) {
     console.warn('notarize skipped');
     return;
