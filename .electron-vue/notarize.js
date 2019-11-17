e4575cd222ba70779896a2dc522e76419887380f
ktmouk <ktmouk@gmail.com>
2019-11-17 19:05:40 +0900

    notarizeの判定にTagを使用するように修正

--- .electron-vue/notarize.js
+++ .electron-vue/notarize.js
@@ -3,7 +3,7 @@ const { notarize } = require('electron-notarize')
 
 const appleId = process.env.APPLE_ID
 const appleIdPassword = process.env.APPLE_PASSWORD
-const pullRequest = process.env.TRAVIS_PULL_REQUEST !== 'false'
+const isReleaseTag = /v[0-9]+\.[0-9]+\.[0-9]+/.test(process.env.TRAVIS_TAG)
 
 const configPath = path.resolve(__dirname, '../package.json')
 const appPath = path.resolve(__dirname, '../build/mac/Hackaru.app')
@@ -11,7 +11,7 @@ const config = require(configPath)
 const appBundleId = config.build.appId
 
 exports.default = async () => {
-  if (!appleId || !appleIdPassword || pullRequest) {
+  if (!appleId || !appleIdPassword || !isReleaseTag) {
     console.warn('notarize skipped');
     return;
   }
