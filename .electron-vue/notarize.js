c6de2307f47ec9b93e62b487194a198fcb1e9bf8
ktmouk <ktmouk@gmail.com>
2019-11-17 19:39:58 +0900

    masterブランチか確認する処理に変更

--- .electron-vue/notarize.js
+++ .electron-vue/notarize.js
@@ -3,7 +3,7 @@ const { notarize } = require('electron-notarize')
 
 const appleId = process.env.APPLE_ID
 const appleIdPassword = process.env.APPLE_PASSWORD
-const pullRequest = process.env.TRAVIS_EVENT_TYPE === 'pull_request'
+const master = process.env.TRAVIS_BRANCH === 'master'
 
 const configPath = path.resolve(__dirname, '../package.json')
 const appPath = path.resolve(__dirname, '../build/mac/Hackaru.app')
@@ -11,8 +11,8 @@ const config = require(configPath)
 const appBundleId = config.build.appId
 
 exports.default = async () => {
-  console.warn(`debug: ${process.env.TRAVIS_EVENT_TYPE}`);
-  if (!appleId || !appleIdPassword || pullRequest) {
+  console.warn(`debug: ${process.env.TRAVIS_BRANCH}`);
+  if (!appleId || !appleIdPassword || !master) {
     console.warn('notarize skipped');
     return;
   }
