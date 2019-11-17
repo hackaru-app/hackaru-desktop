6b29bd7d6d52508c62937c9a4920efa7cb6734d7
ktmouk <ktmouk@gmail.com>
2019-11-17 19:18:15 +0900

    TRAVIS_EVENT_TYPEを使用するように変更

--- .electron-vue/notarize.js
+++ .electron-vue/notarize.js
@@ -3,7 +3,7 @@ const { notarize } = require('electron-notarize')
 
 const appleId = process.env.APPLE_ID
 const appleIdPassword = process.env.APPLE_PASSWORD
-const isReleaseTag = /^v[0-9]+\.[0-9]+\.[0-9]+$/.test(process.env.TRAVIS_TAG)
+const pullRequest = process.env.TRAVIS_EVENT_TYPE === 'pull_request'
 
 const configPath = path.resolve(__dirname, '../package.json')
 const appPath = path.resolve(__dirname, '../build/mac/Hackaru.app')
@@ -11,7 +11,8 @@ const config = require(configPath)
 const appBundleId = config.build.appId
 
 exports.default = async () => {
-  if (!appleId || !appleIdPassword || !isReleaseTag) {
+  console.warn(`debug: ${process.env.TRAVIS_EVENT_TYPE}`);
+  if (!appleId || !appleIdPassword || pullRequest) {
     console.warn('notarize skipped');
     return;
   }
